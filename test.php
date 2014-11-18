223405eaeabb03c39fe22282d48d74c710d18123
sam-blinkdigital <starlord@Sams-Mac-mini.local>
2014-11-18 20:04:53 +0530

    Once again commited.

--- test.php
+++ test.php
@@ -2,4 +2,7 @@
 echo "Added New file to dev branch after master merge with master.";
 echo "Testing the same with cmd";
 echo "One more commit";
+echo "One more commit";
+echo "One more commit";
+
 ?>
